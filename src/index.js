90e1e93b340ac6137ddf70357d291be1d2befd2c
Alberto Yanes <albertoyanesjr3@hotmail.com>
2018-04-23 20:52:43 -0400

    Added Redux and React directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import reducers from './redux/reducers'
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const store = createStore(
+    reducers,
+    compose(
+        applyMiddleware(thunk),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+);
+
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>,
+    document.getElementById('app')
+);
+
 module.hot.accept();
