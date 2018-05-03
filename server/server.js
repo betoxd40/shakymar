169a604f122c7caf6819f9c60e7c04bdf80ee372
kevob1994 <kevob1994@gmail.com>
2018-05-02 20:09:15 -0430

    inicio del api

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/../dist'));
+//app.use(express.static(__dirname + '/../dist'));
+
+app.get('/', function(req, res) {
+  res.send({ name: 'Probando' });
+});
 
 app.listen(3001, function() {
   console.log('server on port 3001');
