7a0934c40990eec610a44e3a3d1d5fcb27feb41d
kevob1994 <kevob1994@gmail.com>
2018-05-07 20:19:06 -0430

    Routes Product

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,33 +1,33 @@
-//EXAMPLE
+// //EXAMPLE
 
-// var BookInstance = require('../models/bookinstance');
+// // var BookInstance = require('../models/bookinstance');
 
-// // Display list of all BookInstances.
+// // // Display list of all BookInstances.
 // exports.bookinstance_list = function(req, res) {
 //   res.send('NOT IMPLEMENTED: BookInstance list');
 // };
 
-// // Display detail page for a specific BookInstance.
+// // // Display detail page for a specific BookInstance.
 // exports.bookinstance_detail = function(req, res) {
 //   res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
 // };
 
-// // Display BookInstance create form on GET.
+// // // Display BookInstance create form on GET.
 // exports.bookinstance_create_get = function(req, res) {
 //   res.send('NOT IMPLEMENTED: BookInstance create GET');
 // };
 
-// // Handle BookInstance create on POST.
+// // // Handle BookInstance create on POST.
 // exports.bookinstance_create_post = function(req, res) {
 //   res.send('NOT IMPLEMENTED: BookInstance create POST');
 // };
 
-// // Display BookInstance delete form on GET.
+// // // Display BookInstance delete form on GET.
 // exports.bookinstance_delete_get = function(req, res) {
 //   res.send('NOT IMPLEMENTED: BookInstance delete GET');
 // };
 
-// // Handle BookInstance delete on POST.
+// // // Handle BookInstance delete on POST.
 // exports.bookinstance_delete_post = function(req, res) {
 //   res.send('NOT IMPLEMENTED: BookInstance delete POST');
 // };
@@ -37,7 +37,7 @@
 //   res.send('NOT IMPLEMENTED: BookInstance update GET');
 // };
 
-// // Handle bookinstance update on POST.
+// //Handle bookinstance update on POST.
 // exports.bookinstance_update_post = function(req, res) {
 //   res.send('NOT IMPLEMENTED: BookInstance update POST');
 // };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
   //res.send({ name: 'Probando' });
-  console.log({ name: 'Probando' });
+  console.log({ name: 'Probando /' });
+});
+
+router.get('/product', function(req, res) {
+  //res.send({ name: 'Probando' });
+  console.log('Probando GET all product');
+});
+
+router.get('/product/:id', function(req, res) {
+  //res.send({ name: 'Probando' });
+  console.log('Probando GET product /product/id');
+});
+
+router.post('/product', function(req, res) {
+  //res.send({ name: 'Probando' });
+  console.log('Probando GET product');
+});
+
+router.put('/product/:id', function(req, res) {
+  //res.send({ name: 'Probando' });
+  console.log('Probando GET product');
+});
+
+router.delete('/product/:id', function(req, res) {
+  //res.send({ name: 'Probando' });
+  console.log('Probando GET product');
 });
 
 module.exports = router;
